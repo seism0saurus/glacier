df9b649d7655f760f4800178e815c58f87d8d551
Ulrich Viefhaus <ulrich.viefhaus@brandad.de>
2024-05-01 14:56:23 +0200

    Fix some tests

--- frontend/src/app/footer/footer.component.spec.ts
+++ frontend/src/app/footer/footer.component.spec.ts
@@ -1,9 +1,11 @@
 import {ComponentFixture, TestBed} from '@angular/core/testing';
 import {FooterComponent} from './footer.component';
 import {HttpClientTestingModule} from "@angular/common/http/testing";
-import {Handle} from "./handle";
 import {FooterService} from "./footer.service";
-import {Observable, of} from "rxjs";
+import {of} from "rxjs";
+import {MatButtonModule} from "@angular/material/button";
+import {MatDialogModule} from "@angular/material/dialog";
+import {MatIconModule} from "@angular/material/icon";
 
 describe('FooterComponent', () => {
   let component: FooterComponent;
@@ -14,7 +16,10 @@ describe('FooterComponent', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [FooterComponent],
-      imports: [HttpClientTestingModule]
+      imports: [HttpClientTestingModule,
+        MatButtonModule,
+        MatIconModule,
+        MatDialogModule]
     });
     fixture = TestBed.createComponent(FooterComponent);
     component = fixture.componentInstance;
@@ -35,45 +40,62 @@ describe('FooterComponent', () => {
     it('should contain the correct text', () => {
       const compiled = fixture.nativeElement as HTMLElement;
       expect(compiled.querySelector('.howto')?.innerHTML)
-        .toContain('You want your toots to be shown here?&nbsp;Mention @unknown@example.com in your toot and use one of the hashtags.');
+        .toContain('You want your toots to be shown here?&nbsp;Mention');
+      expect(compiled.querySelector('.howto')?.innerHTML)
+        .toContain('in your toot and use one of the hashtags.');
+      expect(compiled.querySelector('.howto .handle')?.innerHTML)
+        .toContain('@unknown@example.com');
+    });
+  });
+
+  describe('legal notice and GDPR', () => {
+    it('should exist', () => {
+      const compiled = fixture.nativeElement as HTMLElement;
+      expect(compiled.querySelector('.legal')).toBeTruthy();
+    });
+
+    it('should contain the correct text', () => {
+      const compiled = fixture.nativeElement as HTMLElement;
+      expect(compiled.querySelector('.legal>span')?.innerHTML)
+        .toContain('Legal Notice &amp; GDPR');
     });
   });
 
   describe('copyright', () => {
-      it('should exist', () => {
-        const compiled = fixture.nativeElement as HTMLElement;
-        expect(compiled.querySelector('.copyright')).toBeTruthy();
-      });
-
-      it('should have the correct copyright text', () => {
-        const compiled = fixture.nativeElement as HTMLElement;
-        expect(compiled.querySelector('.copyright')?.innerHTML)
-          .toContain('Glacier is Open Source: ')
-      });
-
-      it('should have the correct url text', () => {
-        const compiled = fixture.nativeElement as HTMLElement;
-        expect(compiled.querySelector('.copyright > a')?.innerHTML)
-          .toBe('https://github.com/seism0saurus/glacier')
-      });
-
-      it('should have the correct repository url', () => {
-        const compiled = fixture.nativeElement as HTMLElement;
-        expect(compiled.querySelector('.copyright > a')?.getAttribute('href'))
-          .toBe('https://github.com/seism0saurus/glacier')
-      });
-
-      it('should open the repository url in a new tab with data protection', () => {
-        const compiled = fixture.nativeElement as HTMLElement;
-        expect(compiled.querySelector('.copyright > a')?.getAttribute('target'))
-          .toBe('_blank')
-        expect(compiled.querySelector('.copyright > a')?.getAttribute('rel'))
-          .toBe('noopener noreferrer')
-      });
+    it('should exist', () => {
+      const compiled = fixture.nativeElement as HTMLElement;
+      expect(compiled.querySelector('.copyright')).toBeTruthy();
     });
 
+    it('should have the correct copyright text', () => {
+      const compiled = fixture.nativeElement as HTMLElement;
+      expect(compiled.querySelector('.copyright')?.innerHTML)
+        .toContain('Glacier is Open Source: ')
+    });
+
+    it('should have the correct url text', () => {
+      const compiled = fixture.nativeElement as HTMLElement;
+      expect(compiled.querySelector('.copyright > a')?.innerHTML)
+        .toBe('https://github.com/seism0saurus/glacier')
+    });
+
+    it('should have the correct repository url', () => {
+      const compiled = fixture.nativeElement as HTMLElement;
+      expect(compiled.querySelector('.copyright > a')?.getAttribute('href'))
+        .toBe('https://github.com/seism0saurus/glacier')
+    });
+
+    it('should open the repository url in a new tab with data protection', () => {
+      const compiled = fixture.nativeElement as HTMLElement;
+      expect(compiled.querySelector('.copyright > a')?.getAttribute('target'))
+        .toBe('_blank')
+      expect(compiled.querySelector('.copyright > a')?.getAttribute('rel'))
+        .toBe('noopener noreferrer')
+    });
+  });
+
   it('should load mastodon handle on init', () => {
-    spy = spyOn(service, 'getMastodonHandle').and.returnValue(of({name:'mastodon@example.com'}));
+    spy = spyOn(service, 'getMastodonHandle').and.returnValue(of({name: 'mastodon@example.com'}));
     component.ngOnInit()
     expect(component.mastodonHandle).toEqual('mastodon@example.com');
   });
