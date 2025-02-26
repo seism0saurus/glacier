95e28d3929a27d48f06adef9d7cc2b75f7a1ebda
Uli <ulrich.viefhaus@brandad.de>
2025-02-26 12:30:46 +0100

    Trying to fix building issues after angular update
    seism0saurus
    
                 #++#
               +--------+
              +------++++                 #
             +--#++++-                   +
            #---+#                       +
            +---+                        .-
            +--+                           #-
            +--+                             #--
       -    +---                               --  -
            +---+                               #-+
            .+---+                               #-+
             +----+                               --#
              +-------+++#.                       --#
     .        #---------------+++-                --#
              #+------------------++             #-+
              ++---------------------+          +-+#
               ++++--------------------+++++++---+
               -++++++-------+---+------------++
                ++++++++-----+++++---+##+###-
         .       ++#+++++----+###+---#
                  ++#####+----# ++---#
                  +-+#    #---#  ++-++
                  +-++   ##+--+   #++++
                 ++-+#    .+--+    ++++
                 +--+#    +---+    ---
                # -++ .   +---#
                          . -.-

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -11,7 +11,10 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env['CI'] ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: process.env['CI'] ? 'dot' : 'html',
+  reporter: [
+    process.env['CI'] ? ['dot'] : ['line'],
+    ['html', { open: 'never', outputFolder: 'playwright-report' }]
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
