8353027bbdbe2cb78bc3c902dac5369b66d49f7e
Uli <ulrich.viefhaus@brandad.de>
2025-02-26 13:48:17 +0100

    Trying to fix building issues after angular update
        seism0saurus
    
                     #++#
                   +--------+
                  +------++++                 #
                 +--#++++-                   +
                #---+#                       +
                +---+                        .-
                +--+                           #-
                +--+                             #--
           -    +---                               --  -
                +---+                               #-+
                .+---+                               #-+
                 +----+                               --#
                  +-------+++#.                       --#
         .        #---------------+++-                --#
                  #+------------------++             #-+
                  ++---------------------+          +-+#
                   ++++--------------------+++++++---+
                   -++++++-------+---+------------++
                    ++++++++-----+++++---+##+###-
             .       ++#+++++----+###+---#
                      ++#####+----# ++---#
                      +-+#    #---#  ++-++
                      +-++   ##+--+   #++++
                     ++-+#    .+--+    ++++
                     +--+#    +---+    ---
                    # -++ .   +---#
                              . -.-

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -19,7 +19,7 @@ export default defineConfig({
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: process.env['BASE_URL'] || 'http://localhost:8080/',
-    headless: process.env['CI'] ? true : false,
+    headless: true,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
