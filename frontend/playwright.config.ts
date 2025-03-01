765f0fd2f2dc8445cfd8b277115f489899d7f335
Uli <ulrich.viefhaus@brandad.de>
2025-03-01 11:19:49 +0100

    Try to get more infos in pipeline, when e2e fails
    seism0saurus
    
                     #++#
                   +--------+
                  +------++++                 #
                 +--#++++-                   +
                #---+#                       +
                +---+                        .-
                +--+                           #-
                +--+                             #--
           -    +---                               --  -
                +---+                               #-+
                .+---+                               #-+
                 +----+                               --#
                  +-------+++#.                       --#
         .        #---------------+++-                --#
                  #+------------------++             #-+
                  ++---------------------+          +-+#
                   ++++--------------------+++++++---+
                   -++++++-------+---+------------++
                    ++++++++-----+++++---+##+###-
             .       ++#+++++----+###+---#
                      ++#####+----# ++---#
                      +-+#    #---#  ++-++
                      +-++   ##+--+   #++++
                     ++-+#    .+--+    ++++
                     +--+#    +---+    ---
                    # -++ .   +---#
                              . -.-

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env['CI'],
   /* Retry on CI only */
-  retries: process.env['CI'] ? 2 : 0,
+  retries: process.env['CI'] ? 1 : 0,
   workers: 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
@@ -19,7 +19,7 @@ export default defineConfig({
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: process.env['BASE_URL'] || 'http://localhost:8080/',
     headless: true,
-
+    screenshot: 'only-on-failure',
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
